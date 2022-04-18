b60f89db7e5e8fb742b1b4b264b497759ac5b728
Artem <martinovich.artem02@mail.ru>
2022-04-18 12:10:01 +0300

    Initial commit

---
+++ src/main/java/com/es/service/Consumer.java
@@ -0,0 +1,35 @@
+package com.es.service;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+@MessageDriven(mappedName = "jms/dest", activationConfig = {
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class Consumer implements MessageListener {
+    private static final Path PATH = Paths.get("E:\\Programs\\glassfish5\\file.txt");
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            String text = textMessage.getText();
+            if (text.endsWith("!")) {
+                Files.write(PATH, (text + '\n').getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            }
+        } catch (JMSException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
